0015c011207e6d23584a677f30bf08a4cfbb3bb9
ABI-DEV-BIO <abimendoza835@gmail.com>
2026-07-10 08:01:34 +0500

    Crear nanobots.py
    :3

---
+++ nanobots.py
@@ -0,0 +1,28 @@
+#Titulo:
+print("REPARTIR NANOBOTS")
+
+#2.Date
+bio_robots=input()
+celulas_dañadas=input()
+
+if estado.lower("100")
+
+#4.Suficientes?
+unidades_usables=bio_robots<celulas_dañadas
+print(unidades_usables)
+
+#5.Alerta activada?
+alerta_sistema=not bio_robots
+print(alerta_sistema)
+
+#6.Igualdad
+print(bio_robots!=celulas_dañadas)
+print(bio_robots==celulas_dañadas)
+
+# Message
+nanobots_dañados = 7
+celulas_curadas = 9
+print(f"{nanobots_dañados} nanobots dañados and {celulas_curadas} celulas curadas")
+
+celulas=40
+print(f"quedan {celulas} celulas")
